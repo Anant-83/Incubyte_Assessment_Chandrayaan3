c2e4fe0af093b49d94c7c328f8896a6dcbb1fc75
Anant <anant832003@gmail.com>
2023-08-29 17:40:28 +0530

    Failed to perform a left turn from the current direction - West, South, East

--- test.js
+++ test.js
@@ -1,10 +1,31 @@
 const Chandrayan = require('./chandrayan');
 describe("Test for the functionality - Turn the spacecraft left/right", () => {
-    test("Turn left from current direction - 'N' ", ()=>{
+    test("Turn left from current direction - 'North' ", ()=>{
         const currentDirection = 'N';
         const currentCoordinates = { x: 0, y: 0, z: 0 };
         const spacecraft = new Chandrayan(currentDirection, currentCoordinates);
         spacecraft.turnLeft();
         expect(spacecraft.direction).toBe('W');
+    })
+    test("Turn left from current direction - 'West' ", ()=>{
+        const currentDirection = 'W';
+        const currentCoordinates = { x: 0, y: 0, z: 0 };
+        const spacecraft = new Chandrayan(currentDirection, currentCoordinates);
+        spacecraft.turnLeft();
+        expect(spacecraft.direction).toBe('S');
+    })
+    test("Turn left from current direction - 'South' ", ()=>{
+        const currentDirection = 'S';
+        const currentCoordinates = { x: 0, y: 0, z: 0 };
+        const spacecraft = new Chandrayan(currentDirection, currentCoordinates);
+        spacecraft.turnLeft();
+        expect(spacecraft.direction).toBe('E');
+    })
+    test("Turn left from current direction - 'East' ", ()=>{
+        const currentDirection = 'E';
+        const currentCoordinates = { x: 0, y: 0, z: 0 };
+        const spacecraft = new Chandrayan(currentDirection, currentCoordinates);
+        spacecraft.turnLeft();
+        expect(spacecraft.direction).toBe('N');
     })
 })
